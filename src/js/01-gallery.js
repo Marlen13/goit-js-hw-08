5b968dd99bb3241c7c30465d9ae14af634264183
Marlen13 <olenamarinova@gmail.com>
2023-04-18 14:45:53 +0300

    add gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -8,4 +8,24 @@ import { galleryItems } from './gallery-items';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
+const gallery = document.querySelector('.gallery');
+(function () {
+  const markup = galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+    )
+    .join('');
+  gallery.insertAdjacentHTML('beforeend', markup);
+})();
+
 console.log('galleryItems');
