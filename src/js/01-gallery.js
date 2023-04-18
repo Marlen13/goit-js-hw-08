adf66823e2291d66b69a72b0f6ed3f432900e454
Marlen13 <olenamarinova@gmail.com>
2023-04-18 14:21:31 +0300

    add library

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,11 @@
+//import library
+import SimpleLightbox from 'simplelightbox';
+
 // Add imports above this line
+//import own code
 import { galleryItems } from './gallery-items';
+//import styles
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
 console.log('galleryItems');
