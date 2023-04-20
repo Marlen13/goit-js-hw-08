13bae8d51a8be1dca4906c981cdd33e28bc54d19
Marlen13 <olenamarinova@gmail.com>
2023-04-20 17:31:42 +0300

    video task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+//import
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Vimeo(iframe);
+const STORAGE_KEY = 'videoplayer-current-time';
+
+console.log(iframe);
+console.log(player);
+// console.log(STORAGE_KEY);
+
+player.on(
+  'timeupdate',
+  throttle(function (data) {
+    localStorage.setItem('STORAGE_KEY', JSON.stringify(data.seconds));
+  }, 1000)
+);
+
+player.setCurrentTime(localStorage.getItem('STORAGE_KEY'));
