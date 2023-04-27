1b069d412054420f46cde6abe37aaa917d193691
Marlen13 <olenamarinova@gmail.com>
2023-04-27 20:08:32 +0300

    update task3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -6,35 +6,42 @@ const textarea = document.querySelector('[name = "message"]');
 const btn = document.querySelector('button');
 btn.setAttribute('disabled', 'disabled');
 const STORAGE_KEY = 'feedback-form-state';
-
-form.addEventListener('input', throttle(onInput, 500));
-form.addEventListener('submit', onSubmit);
-
 const arrForm = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
-
+//check localStorage
+if (arrForm.email) {
+  input.value = arrForm.email;
+}
+if (arrForm.message) {
+  textarea.value = arrForm.message;
+}
+//listen input event
+form.addEventListener('input', throttle(onInput, 500));
 function onInput(evt) {
+  arrForm[evt.target.name] = evt.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(arrForm));
+
   if (!evt.target.value) {
     btn.disabled = true;
   } else {
     btn.disabled = false;
-    arrForm[evt.target.name] = evt.target.value;
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(arrForm));
   }
 }
-checkLocal(arrForm);
-
-function checkLocal({ email, message }) {
-  if (email) {
-    input.value = email;
-  }
-  if (message) {
-    textarea.value = message;
-  }
-}
-
+//listen submit event
+form.addEventListener('submit', onSubmit);
 function onSubmit(evt) {
   evt.preventDefault();
   localStorage.removeItem(STORAGE_KEY);
   form.reset();
   btn.disabled = true;
 }
+
+// checkLocal(arrForm);
+
+// function checkLocal({ email, message }) {
+//   if (email) {
+//     input.value = email;
+//   }
+//   if (message) {
+//     textarea.value = message;
+//   }
+// }
