22d42f5e8950e6f684bba18cac4a2b0243ca425b
Marlen13 <olenamarinova@gmail.com>
2023-04-27 16:15:08 +0300

    update task3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,6 +3,8 @@ import throttle from 'lodash.throttle';
 const form = document.querySelector('.feedback-form');
 const input = document.querySelector('[name = "email"]');
 const textarea = document.querySelector('[name = "message"]');
+const btn = document.querySelector('button');
+btn.setAttribute('disabled', 'disabled');
 const STORAGE_KEY = 'feedback-form-state';
 
 form.addEventListener('input', throttle(onInput, 500));
@@ -11,9 +13,13 @@ form.addEventListener('submit', onSubmit);
 const arrForm = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
 
 function onInput(evt) {
-  arrForm[evt.target.name] = evt.target.value.trim();
-  arrForm[evt.target.name] = evt.target.value;
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(arrForm));
+  if (!evt.target.value) {
+    btn.disabled = true;
+  } else {
+    btn.disabled = false;
+    arrForm[evt.target.name] = evt.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(arrForm));
+  }
 }
 checkLocal(arrForm);
 
@@ -28,9 +34,6 @@ function checkLocal({ email, message }) {
 
 function onSubmit(evt) {
   evt.preventDefault();
-  const arrForm = localStorage.getItem(STORAGE_KEY);
-  console.log(arrForm);
   localStorage.removeItem(STORAGE_KEY);
-  input.value = '';
-  textarea.value = '';
+  form.reset();
 }
